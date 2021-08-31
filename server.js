0f6747fbd0e5e6f40564febcc7275ba00d7a6518
Dahlton <dahlton@linkd.dev>
2021-08-30 21:21:35 -0400

    changed port from env to config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
-const port = process.env.port || 3001;
+const config = require('config')
+const port = config.get('port') || 3001;
 const connectDB = require('./config/db')
 
 
