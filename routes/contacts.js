ca188b418647383441d1786608fa1e3ee0ab84c1
Dahlton <dahlton@linkd.dev>
2021-08-30 21:15:15 -0400

    Finished up contacts api

--- routes/contacts.js
+++ routes/contacts.js
@@ -21,26 +21,58 @@ router.get("/", auth, async (req, res) => {
 // @route POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post("/",[
-    check('name', 'Please enter a valid name').not().isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-], (req, res) => {
-	res.send("Apple jacks contacts");
+router.post("/", [auth, [check("name", "Please enter a valid name").not().isEmpty()]], async (req, res) => {
+	const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+	try {
+		contact = new Contact({...req.body, user: req.user.id});
+		await contact.save();
+	} catch (error) {
+		console.error(error)
+		res.status(500).send('Server error')
+	}
+	res.json(contact)
 });
 
 // @route PUT api/contacts/:id
 // @desc Update Contact
 // @access Private
-router.put("/:id", auth, (req, res) => {
-	res.send("Apple jacks contacts - put");
+router.put("/:id", [auth, [check("name", "Please enter a valid name").not().isEmpty()]], async (req, res) => {
+	const {id} = req.params
+	try {
+		const contact = await Contact.findById(id)
+		if(!contact){
+			return res.status(404).json({ msg: 'Contact not found' })
+		}
+		if(contact.user.toString() !== req.user.id){
+			return res.status(401).json({ msg: 'Not authorized'})
+		}
+		await Contact.findByIdAndUpdate(id, {...req.body}, {new:true})
+	} catch (error) {
+		console.error(error)
+		res.status(500).send('Server error')
+	}
+	res.json(contact)
 });
 
 // @route DELETE api/contacts/:id
 // @desc Delete Contact
 // @access Private
-router.delete("/:id", (req, res) => {
-	res.send("Apple jacks contacts - delete");
+router.delete("/:id", [auth, [check("name", "Please enter a valid name").not().isEmpty()]], async (req, res) => {
+	const {id} = req.params
+	try {
+		Contact.findByIdAndRemove(id, (error) => {
+			if(error){
+				console.error(error)
+			}
+			return res.json({msg: 'Deleted'})
+		})
+	} catch (error) {
+		console.error(error)
+		return res.status(500).send('Server error')
+	}
 });
 
 module.exports = router;
