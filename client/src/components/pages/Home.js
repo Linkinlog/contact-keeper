3ef17f8732b627c5b87a6b3f73f6e7e4555a6bce
Dahlton <dahlton@linkd.dev>
2021-09-02 22:13:53 -0400

    Filled home out

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Contacts } from '../contacts/Contacts';
+import { ContactsForm } from '../contacts/ContactsForm';
 
 export const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className='container'>
+			<div className='row mt-5 g-5 row-cols-2'>
+				<div className='col-md-5'>
+					<ContactsForm />
+				</div>
+				<div className='col-md-6 offset-md-1'>
+					<Contacts />
+				</div>
+			</div>
 		</div>
-	)
-}
+	);
+};
