ae9dc8ba57bcdd1bcd1370f83f8a20fe6c645da8
Dahlton <dahlton@linkd.dev>
2021-09-02 22:14:20 -0400

    fixed uuid import, set up add and delete functions

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import contactCotext from './contactContext';
 import contactReducer from './contactReducer';
 import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
@@ -27,16 +27,21 @@ const ContactState = (props) => {
 				email: 'harry@gmail.com',
 				phone: '333-333-3333',
 				type: 'professional',
-			}
+			},
 		],
 	};
 
-	const [ state, dispatch ] = useReducer(contactReducer, initialState);
-	
+	const [state, dispatch] = useReducer(contactReducer, initialState);
+
 	// Add Contact
-
+	const addContact = (contact) => {
+		contact.id = uuid();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 	// Delete Contact
-
+	const deleteContact = (id) => {
+		dispatch({ type: DELETE_CONTACT, payload: id})
+	};
 	// Set Current Contact
 
 	// Clear Current Contact
@@ -46,14 +51,17 @@ const ContactState = (props) => {
 	// Filter Contacts
 
 	// Clear Filter
-	return(
+	return (
 		<contactCotext.Provider
 			value={{
-				contacts: state.contacts
-			}}>
-			{ props.children }
+				contacts: state.contacts,
+				addContact,
+				deleteContact,
+			}}
+		>
+			{props.children}
 		</contactCotext.Provider>
-	)
+	);
 };
 
 export default ContactState;
