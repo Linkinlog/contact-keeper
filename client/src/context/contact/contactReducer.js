67e8de555711ba9248c8e9b793f6f832fc228766
Dahlton <dahlton@linkd.dev>
2021-09-02 23:13:42 -0400

    set up update/set/clear reducer functions

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,19 +1,35 @@
 import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
 
-// eslint-disable-next-line 
+// eslint-disable-next-line
 export default (state, action) => {
-	switch(action.type) {
+	switch (action.type) {
 		case ADD_CONTACT:
 			return {
 				...state,
-				contacts: [...state.contacts, action.payload]
-			}
+				contacts: [...state.contacts, action.payload],
+			};
+			case UPDATE_CONTACT:
+				return {
+					...state,
+					contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact ),
+				};
 		case DELETE_CONTACT:
 			return {
 				...state,
-				contacts: state.contacts.filter(item => item.id !== action.payload)
-			}
+				contacts: state.contacts.filter((item) => item.id !== action.payload),
+			};
+		case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload,
+			};
+		case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null,
+			};
+			
 		default:
 			return state;
 	}
-}
+};
