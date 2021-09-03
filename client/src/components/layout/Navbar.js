a6e0479467abcadabc6cd3feaf2728e3ee9f74a5
Dahlton <dahlton@linkd.dev>
2021-09-02 22:13:31 -0400

    refactored nav for react

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = ({ title, icon }) => {
 	return (
-		<nav class='navbar navbar-expand-md navbar-dark bg-primary'>
-			<div class='container-fluid'>
-				<Link class='navbar-brand' to='/'>
+		<nav  className='navbar navbar-expand-md navbar-dark bg-primary'>
+			<div  className='container-fluid'>
+				<Link  className='navbar-brand' to='/'>
 					<i className={icon} /> {title}
 				</Link>
-				<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-					<span class='navbar-toggler-icon'></span>
+				<button  className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+					<span  className='navbar-toggler-icon'></span>
 				</button>
-				<div class='collapse navbar-collapse' id='navbarNav'>
+				<div  className='collapse navbar-collapse' id='navbarNav'>
 					<ul className='navbar-nav'>
 						<li className='nav-item'>
 							<Link className='nav-link' to='/'>
