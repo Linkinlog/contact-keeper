10194b4f01a29d86ef39724eaad2de559b4dc3cb
Dahlton <dahlton@linkd.dev>
2021-09-09 21:38:48 -0400

    added react transitions

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import contactCotext from '../../context/contact/contactContext';
 import { ContactItem } from './ContactItem';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 export const Contacts = () => {
 	const contactContext = useContext(contactCotext);
+	const { contacts, filtered } = contactContext;
 
-	const { contacts } = contactContext;
+	if (contacts.length === 0) {
+		return <h4>Please add a contact</h4>;
+	}
 
 	return (
-		<div className="row g-5 row-cols-xl-2">
-			{contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)}
-		</div>
+		<Fragment>
+			<TransitionGroup>
+				{filtered !== null ? filtered.map((contact) => <CSSTransition key={contact.id} timeout={500} classNames='item' ><ContactItem contact={contact} /></CSSTransition>) : contacts.map((contact) => <CSSTransition key={contact.id} timeout={500} classNames='item' ><ContactItem contact={contact} /></CSSTransition>)}
+			</TransitionGroup>
+		</Fragment>
 	);
 };
