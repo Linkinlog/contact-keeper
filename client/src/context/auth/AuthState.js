1a31467f9159254e27c948ca4a9e0dc93960d740
Dahlton <dahlton@linkd.dev>
2021-09-11 23:25:24 -0400

    set up authcontext state

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import axios from 'axios'
 import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_ERRORS } from '../types';
 
 const AuthState = (props) => {
@@ -15,14 +16,52 @@ const AuthState = (props) => {
 	const [state, dispatch] = useReducer(authReducer, initialState);
 
 	// Load user
-
+	const loadUser = () => {
+		console.log('object');
+	}
+	
 	// Register user
+	const register = async formData => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+		try {
+			const res = await axios.post( '/api/users' , formData , config )
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data
+			})
+		} catch (error) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: error.response.data.msg
+			})
+		}
+	}
 
 	// Login user
+	const login = async () => {
+		//todo load token into global headers
+		try {
+			const res = await axios.get('/api/auth');
+			dispatch({
+				type: USER_LOADED,
+				payload: res.data
+			})
+		} catch (error) {
+			dispatch({ type: AUTH_ERROR })
+		}
+	}
 
 	// Logout
+	const logout = () => {
+		console.log('object');
+	}
 
 	// Clear errors
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS})
 
 	return (
 		<AuthContext.Provider
@@ -32,6 +71,11 @@ const AuthState = (props) => {
 				loading: state.loading,
 				error: state.error,
 				user: state.user,
+				register,
+				loadUser,
+				login,
+				logout,
+				clearErrors
 			}}
 		>
 			{props.children}
