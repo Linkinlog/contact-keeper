ea10c43deb22e7c4a8983af9e35d0aa79dd88837
Dahlton <dahlton@linkd.dev>
2021-09-11 23:28:52 -0400

    set up onsubmit action for the form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
+
 
 export const Register = () => {
+	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
+	const { register, error, clearErrors } = authContext
+	const { setAlert } = alertContext;
+
+	useEffect(() => {
+		if(error === 'User already exists'){
+			setAlert(error, 'danger')
+			clearErrors();
+		}
+	}, [error])
+
 	const [user, setUser] = useState({
 		name: '',
 		email: '',
@@ -10,31 +25,57 @@ export const Register = () => {
 
 	const { name, email, password, password2 } = user;
 
-	const onChange = e => {
-		setUser({...user, [e.target.name]: e.target.value})
-	}
-	
+	const onSubmit = (e) => {
+		e.preventDefault();
+		let err = false;
+		for (let field in user) {
+			if (field === 'password2') continue;
+			if (user[field] === '') {
+				setAlert(`${field.charAt(0).toUpperCase() + field.slice(1)} is required.`, 'warning');
+				err = true
+			}
+		}
+		if(password !== password2){
+			setAlert('Passwords do not match', 'warning')
+			err = true
+		}
+		if(!err){
+			register({
+				name,
+				email,
+				password
+			})
+			setAlert('Register Successful', 'info')
+		}
+	};
+
+	const onChange = (e) => {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div className='col'>
-			<h1>
+			<h1 className='text-center'>
 				Account <span className='text-primary'>Register</span>
 			</h1>
-			<form>
-				<label className='form-label' htmlFor='name'>Name</label>
+			<form onSubmit={onSubmit}>
+				<label className='form-label' htmlFor='name'>
+					Name
+				</label>
 				<input type='text' name='name' value={name} className='form-control' onChange={onChange} />
-			</form>
-			<form>
-				<label className='form-label' htmlFor='email'>Email Address</label>
+				<label className='form-label' htmlFor='email'>
+					Email Address
+				</label>
 				<input type='email' name='email' value={email} className='form-control' onChange={onChange} />
-			</form>
-			<form>
-				<label className='form-label' htmlFor='password'>Password</label>
+				<label className='form-label' htmlFor='password'>
+					Password
+				</label>
 				<input type='password' name='password' value={password} className='form-control' onChange={onChange} />
-			</form>
-			<form>
-				<label className='form-label' htmlFor='password2'>Confirm Password</label>
-				<input type='password2' name='password2' value={password2} className='form-control' onChange={onChange} />
-				<input type="submit" value="Register" className='btn btn-primary' />
+				<label className='form-label' htmlFor='password2'>
+					Confirm Password
+				</label>
+				<input type='password2' name='password2' value={password2} className='form-control mb-3' onChange={onChange} />
+				<input type='submit' value='Register' className='btn btn-primary' />
 			</form>
 		</div>
 	);
