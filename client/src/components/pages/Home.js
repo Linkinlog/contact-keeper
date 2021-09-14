4026b56e78e9e99209fb7353a4bc5825ec80c013
Dahlton <dahlton@linkd.dev>
2021-09-13 20:27:51 -0400

    pulled in auth context and auth checking

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Contacts } from '../contacts/Contacts';
 import { ContactsForm } from '../contacts/ContactsForm';
 import { ContactFilter } from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
+
 export const Home = () => {
+	const authContext = useContext(AuthContext)
+	const { loadUser } = authContext
+	useEffect(() => {
+		loadUser();
+		// eslint-disable-next-line
+	}, [])
 	return (
 		<div className='container'>
 			<div className='row mt-5 g-5 row-cols-2'>
