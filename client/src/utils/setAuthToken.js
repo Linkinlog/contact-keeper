6e981f6e73232fec13c431ddf6188e09a006505d
Dahlton <dahlton@linkd.dev>
2021-09-14 21:59:46 -0400

    pointed delete to proper value

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -4,6 +4,6 @@ export const setAuthToken = token => {
 	if(token){
 		axios.defaults.headers.common['x-auth-token'] = token;
 	}else {
-		delete axios.defaults.header.common['x-auth-token']
+		delete axios.defaults.headers.common['x-auth-token']
 	}
 }
