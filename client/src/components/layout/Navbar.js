f506b5d977d6d908a198cec07e31b69e7aeb4672
Dahlton <dahlton@linkd.dev>
2021-09-15 21:24:43 -0400

    Added user name to nav and prettied up

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
 export const Navbar = ({ title, icon }) => {
-	const authContext = useContext(AuthContext)
-	const {isAuthenticated} = authContext
+	const authContext = useContext(AuthContext);
+	const { isAuthenticated, user } = authContext;
 	return (
-		<nav  className='navbar navbar-expand-md navbar-dark bg-primary'>
-			<div  className='container-fluid'>
-				<Link  className='navbar-brand' to='/'>
+		<nav className='navbar navbar-expand-md navbar-dark hotBorder bg-primary'>
+			<div className='container-fluid'>
+				<Link className='navbar-brand' to='/'>
 					<i className={icon} /> {title}
 				</Link>
-				<button  className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-					<span  className='navbar-toggler-icon'></span>
+				<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+					<span className='navbar-toggler-icon'></span>
 				</button>
-				<div  className='collapse navbar-collapse' id='navbarNav'>
+				<div className='collapse navbar-collapse' id='navbarNav'>
 					<ul className='navbar-nav'>
 						<li className='nav-item'>
 							<Link className='nav-link' to='/'>
@@ -28,16 +28,29 @@ export const Navbar = ({ title, icon }) => {
 							</Link>
 						</li>
 					</ul>
-					{!isAuthenticated &&
-										<ul className="navbar-nav ms-auto">
-										<li className="nav-item">
-											<Link className="nav-link" to='/login'>Login</Link>
-										</li>
-										<li className="nav-item">
-											<Link className="nav-link" to='/register'>Register</Link>
-										</li>
-									</ul>
-					}
+					{!isAuthenticated && (
+						<ul className='navbar-nav ms-auto'>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/login'>
+									Login
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link className='nav-link' to='/register'>
+									Register
+								</Link>
+							</li>
+						</ul>
+					)}
+					{isAuthenticated && (
+						<ul className='navbar-nav ms-auto'>
+							<li className='nav-item'>
+								<Link to='/logout' className='nav-link'>
+									Logout {user && `(${user.name})`}
+								</Link>
+							</li>
+						</ul>
+					)}
 				</div>
 			</div>
 		</nav>
