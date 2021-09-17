b4403e6d0dd979c63e86f2fd86393ba483491b18
Dahlton <dahlton@linkd.dev>
2021-09-16 22:16:23 -0400

    removed bunk route, set up check for production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const config = require('config')
 const port = config.get('port') || 3001;
 const connectDB = require('./config/db')
+const path = require('path')
 
 
 app.use(express.json({extended: false}))
@@ -10,14 +11,14 @@ app.use(express.json({extended: false}))
 // Connect to db
 connectDB();
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Meowdy!'});
-});
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
