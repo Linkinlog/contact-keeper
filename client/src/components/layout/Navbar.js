376f784bf62ac963ec9439b352d2306259a84ada
Dahlton <dahlton@linkd.dev>
2021-09-17 10:25:28 -0400

    improved nav interaction on mobile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,7 +19,7 @@ export const Navbar = ({ title, icon }) => {
 				<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
 					<span className='navbar-toggler-icon'></span>
 				</button>
-				<div onClick={onClick} ref={nav} className='collapse navbar-collapse' id='navbarNav'>
+				<div onTouchEnd={onClick} onClick={onClick} ref={nav} className='collapse navbar-collapse' id='navbarNav'>
 					<ul className='navbar-nav'>
 						<li className='nav-item'>
 							<Link className='nav-link' to='/'>
