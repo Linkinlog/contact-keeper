b5c6349166fc9d56cc5763fe7ce1a7d4277584ca
Dahlton <dahlton@linkd.dev>
2021-09-16 22:10:42 -0400

    added spinner

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import spinner from './spinner.gif'
+export const Spinner = () => <React.Fragment>
+            <img src={spinner} alt="Loading..." style={{width: '200px', margin:'auto', display:'block'}} />
+        </React.Fragment>
+
