dedf016fcaf3f65c989e16a45def52f36ebca455
Dahlton <dahlton@linkd.dev>
2021-09-16 22:15:28 -0400

    refactored to use mongo instead of local

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,70 +1,90 @@
 import React, { useReducer } from 'react';
-import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
-import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
+import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CONTACTS, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER, CONTACT_ERROR, GET_CONTACTS } from '../types';
+import axios from 'axios';
 
 const ContactState = (props) => {
 	const initialState = {
-		contacts: [
-			{
-				id: 1,
-				name: 'Jill Johnson',
-				email: 'jill@gmail.com',
-				phone: '111-111-1111',
-				type: 'personal',
-			},
-			{
-				id: 2,
-				name: 'Sara Watson',
-				email: 'sara@gmail.com',
-				phone: '222-222-2222',
-				type: 'personal',
-			},
-			{
-				id: 3,
-				name: 'Harry Johnson',
-				email: 'harry@gmail.com',
-				phone: '333-333-3333',
-				type: 'professional',
-			},
-		],
+		contacts: null,
 		current: null,
-		filtered: null
+		filtered: null,
+		error: null,
 	};
 
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
-	// Add Contact
-	const addContact = (contact) => {
-		contact.id = uuid();
-		dispatch({ type: ADD_CONTACT, payload: contact });
+	// Get contacts
+	const getContacts = async () => {
+		try {
+			const res = await axios.get('/api/contacts');
+			dispatch({ type: GET_CONTACTS, payload: res.data });
+		} catch (error) {
+			dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+		}
 	};
+
+	// Add Contact
+	const addContact = async (contact) => {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+			},
+		};
+		try {
+			const res = await axios.post('/api/contacts', contact, config);
+			dispatch({ type: ADD_CONTACT, payload: res.data });
+		} catch (error) {
+			dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+		}
+	};
+
 	// Delete Contact
-	const deleteContact = (id) => {
-		dispatch({ type: DELETE_CONTACT, payload: id})
+	const deleteContact = async (id) => {
+		try {
+			await axios.delete(`/api/contacts/${id}`);
+			dispatch({ type: DELETE_CONTACT, payload: id });
+		} catch (error) {
+			dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+		}
 		clearCurrent();
 	};
+
 	// Set Current Contact
-	const setCurrent = contact => {
-		dispatch({type: SET_CURRENT, payload: contact})
-	}
+	const setCurrent = (contact) => {
+		dispatch({ type: SET_CURRENT, payload: contact });
+	};
 	// Clear Current Contact
 	const clearCurrent = () => {
-		dispatch({type: CLEAR_CURRENT})
-	}
+		dispatch({ type: CLEAR_CURRENT });
+	};
 	// Update Contact
-	const updateContact = contact => {
-		dispatch({type: UPDATE_CONTACT, payload: contact})
-	}
+	const updateContact = async contact => {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+			},
+		};
+		try {
+			const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+			console.log(res.data)
+			dispatch({ type: UPDATE_CONTACT, payload: res.data });
+		} catch (error) {
+			dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+		}
+	};
 	// Filter Contacts
-	const filterContacts = text => {
-		dispatch({ type: FILTER_CONTACTS, payload: text})
-	}
+	const filterContacts = (text) => {
+		dispatch({ type: FILTER_CONTACTS, payload: text });
+	};
 	// Clear Filter
 	const clearFilter = () => {
-		dispatch({ type: CLEAR_FILTER })
-	}
+		dispatch({ type: CLEAR_FILTER });
+	};
+	// Clear Contacts
+	const clearContacts = () => {
+		dispatch({ type: CLEAR_CONTACTS });
+	};
 	return (
 		<ContactContext.Provider
 			value={{
@@ -76,8 +96,11 @@ const ContactState = (props) => {
 				updateContact,
 				filterContacts,
 				clearFilter,
+				getContacts,
+				clearContacts,
 				current: state.current,
 				filtered: state.filtered,
+				error: state.error,
 			}}
 		>
 			{props.children}
